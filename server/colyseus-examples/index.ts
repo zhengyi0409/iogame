1c8527ad154823a9b69ed4cfaacb7e69c417b4ab
zhengyi0409 <13925205491@163.com>
2020-03-03 11:17:58 +0800

    提交服务端

---
+++ server/colyseus-examples/index.ts
@@ -0,0 +1,58 @@
+import path from 'path';
+import express from 'express';
+import serveIndex from 'serve-index';
+import cors from 'cors';
+import { createServer } from 'http';
+import { Server } from 'colyseus';
+import { monitor } from '@colyseus/monitor';
+
+// Import demo room handlers
+import { ChatRoom } from "./rooms/01-chat-room";
+import { StateHandlerRoom } from "./rooms/02-state-handler";
+import { AuthRoom } from "./rooms/03-auth";
+
+const port = Number(process.env.PORT || 2567) + Number(process.env.NODE_APP_INSTANCE || 0);
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Attach WebSocket Server on HTTP Server.
+const gameServer = new Server({
+  server: createServer(app),
+  express: app,
+});
+
+// Register ChatRoom as "chat"
+gameServer.define("chat", ChatRoom);
+
+// Register ChatRoom with initial options, as "chat_with_options"
+// onInit(options) will receive client join options + options registered here.
+gameServer.define("chat_with_options", ChatRoom, {
+    custom_options: "you can use me on Room#onCreate"
+});
+
+// Register StateHandlerRoom as "state_handler"
+gameServer.define("state_handler", StateHandlerRoom);
+
+// Register StateHandlerRoom as "state_handler"
+gameServer.define("auth", AuthRoom);
+
+app.use('/', express.static(path.join(__dirname, "static")));
+app.use('/', serveIndex(path.join(__dirname, "static"), {'icons': true}))
+
+// (optional) attach web monitoring panel
+app.use('/colyseus', monitor());
+
+gameServer.onShutdown(function(){
+  console.log(`game server is going down.`);
+});
+
+gameServer.listen(port);
+
+// process.on("uncaughtException", (e) => {
+//   console.log(e.stack);
+//   process.exit(1);
+// });
+
+console.log(`Listening on http://localhost:${ port }`);
